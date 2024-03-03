a5371290b6665ed5876a69e96d6411f30baf03d5
Mr. Hasan <hasanshahriar32@gmail.com>
2024-03-03 13:11:42 +0600

    u

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView>
+        <Text style={{ fontWeight: "bold", fontSize: 20, margin: 5 }}>
+          Open up App.js to start working on your app!
+        </Text>
+        <Text>Hello world!</Text>
+        <StatusBar style="auto" />
+        <Image
+          source={require("./assets/adaptive-icon.png")}
+          style={{ height: 200, width: 200 }}
+        />
+        <View style={styles.imageBtn}>
+          <Image
+            source={{
+              uri: "https://avatars.githubusercontent.com/u/67302140?v=4",
+            }}
+            style={{ width: 30, height: 30 }}
+          />
+          <Button title="Click me" onPress={() => alert("Button clicked")} />
+        </View>
+        <Text style={styles.textStyle}>
+          {new Array(20).fill(0).map((_, index) => (
+            <Text key={index}>This is a text {index}</Text>
+          ))}
+        </Text>
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +43,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    color: "#fff",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  imageBtn: {
+    display: "flex",
+    flexDirection: "row",
+    width: "full",
+  },
+  textStyle: { fontWeight: "bold", fontSize: 20, margin: 5 },
 });
