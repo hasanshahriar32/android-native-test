e74be04ea25b634621bb7445d5d2097a8c831248
Mr. Hasan <hasanshahriar32@gmail.com>
2024-03-14 13:42:49 +0600

    Add React hooks and update styles

--- App.js
+++ App.js
@@ -7,51 +7,30 @@ import {
   Text,
   View,
 } from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ScrollView>
-        <Text style={{ fontWeight: "bold", fontSize: 20, margin: 5 }}>
-          Open up App.js to start working on your app!
-        </Text>
-        <Text>Hello world!</Text>
-        <StatusBar style="auto" />
-        <Image
-          source={require("./assets/adaptive-icon.png")}
-          style={{ height: 200, width: 200 }}
-        />
-        <View style={styles.imageBtn}>
-          <Image
-            source={{
-              uri: "https://avatars.githubusercontent.com/u/67302140?v=4",
-            }}
-            style={{ width: 30, height: 30 }}
-          />
-          <Button title="Click me" onPress={() => alert("Button clicked")} />
-        </View>
-        <Text style={styles.textStyle}>
-          {new Array(20).fill(0).map((_, index) => (
-            <Text key={index}>This is a text {index}</Text>
-          ))}
-        </Text>
-      </ScrollView>
+    <View style={style.container}>
+      <Text style={style.textOne}>
+        Open up App.js to start working on your app!
+      </Text>
+      <StatusBar style="auto" />
+      <Button title="Click Me" onPress={() => alert("Button Clicked")} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
-    flex: 1,
-    color: "#fff",
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    height: 300,
+    width: 380,
+    backgroundColor: "dodgerblue",
   },
-  imageBtn: {
-    display: "flex",
-    flexDirection: "row",
-    width: "full",
+  textOne: {
+    color: "white",
+    fontSize: 30,
+    textAlign: "center",
+    marginTop: 100,
   },
-  textStyle: { fontWeight: "bold", fontSize: 20, margin: 5 },
 });
